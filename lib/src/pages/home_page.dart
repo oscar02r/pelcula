ff38801ab0348a19e5294491dd7e2a930e14df79
oscar02r <oscary.ramirez@gmail.com>
2020-04-24 06:27:56 +0200

    PageView is ready

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class Homepage extends StatelessWidget {
   
@@ -59,14 +60,25 @@ class Homepage extends StatelessWidget {
         return Container(
           width: double.infinity,
           child:Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text('Populares', style:Theme.of(context).textTheme.subhead),
-
+              Container(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Text('Populares', style:Theme.of(context).textTheme.subhead)
+              ),
+              SizedBox(height: 5.0,),
               FutureBuilder(
                 future: peliculasProvider.getPolular(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   //snapshot.data?.forEach( (p) => print(p.title) );
-                  return Text('sata');
+                   if (snapshot.hasData) {
+                     
+                      return MovieHorizontal(peliculas: snapshot.data);
+                      
+                   } else {
+
+                     return  Center(child: CircularProgressIndicator());
+                   }
                 },
               ),
             ],

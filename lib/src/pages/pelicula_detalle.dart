40a19f5cad21587798b0a737805a3bc4b370a417
oscar02r <oscary.ramirez@gmail.com>
2020-04-29 23:10:04 +0200

    Adding hero moviehorizontal and pelicula detail

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/actores_model.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 
 class PeliculaDetalle extends StatelessWidget {
 
@@ -8,98 +10,144 @@ class PeliculaDetalle extends StatelessWidget {
     final Pelicula pelicula = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-          body: CustomScrollView(
-            slivers: <Widget>[
+        body: CustomScrollView(
+      slivers: <Widget>[
+        _crearAppbar(pelicula),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            SizedBox(height: 10.0),
+            _posterTitulo(context, pelicula),
+            _descripcion(pelicula),
+            _crearCasting(pelicula),
+          ]),
+        )
+      ],
+    ));
+  }
 
-              _crearAppbar(pelicula),
-              SliverList(
-                delegate: SliverChildListDelegate(
-                [
-                  SizedBox(height: 10.0),
-                  _posterTitulo( context, pelicula ),
-                  _descripcion(pelicula),
-                
-                ]
-              ),
-              
-               )
-            ],
-          )
+  Widget _crearAppbar(Pelicula pelicula) {
+    return SliverAppBar(
+      elevation: 2.0,
+      backgroundColor: Colors.indigoAccent,
+      expandedHeight: 200.0,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        collapseMode: CollapseMode.parallax, 
+        title: Text(
+          pelicula.title,
+          style: TextStyle(color: Colors.white, fontSize: 16.0),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/img/loading.gif'),
+          image: NetworkImage(pelicula.getBackgroundImg()),
+          fit: BoxFit.cover,
+          fadeInDuration: Duration(milliseconds: 150),
+        ),
+      ),
     );
   }
 
- Widget _crearAppbar(Pelicula pelicula) {
-
-        return SliverAppBar(
-               elevation: 2.0,
-               backgroundColor: Colors.indigoAccent,
-               expandedHeight: 200.0,
-               floating: false,
-               pinned: true,
-               flexibleSpace: FlexibleSpaceBar(
-                   centerTitle: true,
-                   title: Text(
-                     pelicula.title,
-                     style: TextStyle(color:Colors.white, fontSize: 16.0),
-                   ),
-                 background: FadeInImage(
-                   placeholder: AssetImage('assets/img/loading.gif'),
-                    image: NetworkImage(pelicula.getBackgroundImg()),
-                    fit: BoxFit.cover,
-                    fadeInDuration: Duration(milliseconds: 150),
-                    ),
-               ),
-        );
-  }
-
-  Widget _posterTitulo(BuildContext context,Pelicula pelicula) {
-     return Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20.0),
-                   child: Image(
-                  image: NetworkImage(pelicula.getPosterImg()),
-                  height: 150.0,
-                ),
+  Widget _posterTitulo(BuildContext context, Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          Hero(
+              tag: pelicula.id,
+              child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image(
+                image: NetworkImage(pelicula.getPosterImg()),
+                height: 150.0,
               ),
-              SizedBox(width: 20.0),
-              Flexible(
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      pelicula.title,
-                      style: Theme.of(context).textTheme.title
-                      ),
-                      Text(
-                        pelicula.originalTitle,
-                        style: Theme.of(context).textTheme.subhead, 
-                        ),
-
-                        Row(
-                          children: <Widget>[
-                            Icon(Icons.star_border),
-                            Text(
-                              pelicula.voteAverage.toString(),
-                              style: Theme.of(context).textTheme.subhead,
-                            )
-                          ],
-                        )
-
-                  ],
-                )
-                )
-            ],
+            ),
           ),
-     );
+          SizedBox(width: 20.0),
+          Flexible(
+              child: Column(
+            children: <Widget>[
+              Text(pelicula.title, style: Theme.of(context).textTheme.title),
+              Text(
+                pelicula.originalTitle,
+                style: Theme.of(context).textTheme.subhead,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.star_border),
+                  Text(
+                    pelicula.voteAverage.toString(),
+                    style: Theme.of(context).textTheme.subhead,
+                  )
+                ],
+              )
+            ],
+          ))
+        ],
+      ),
+    );
   }
 
   Widget _descripcion(Pelicula pelicula) {
-         return Container(
-           padding: EdgeInsets.symmetric(vertical:20.0, horizontal:10),
-           child: Text(pelicula.overview,
-           textAlign: TextAlign.justify,),
-         );    
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10),
+      child: Text(
+        pelicula.overview,
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
+
+  Widget _crearCasting(Pelicula pelicula) {
+    final peliProvider = PeliculasProvider();
+
+    return FutureBuilder(
+      future: peliProvider.getCast(pelicula.id.toString()),
+      builder: (context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return _crearActoresPageView(snapshot.data);
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  Widget _crearActoresPageView(List<Actor> actores) {
+    return SizedBox(
+      height: 200.0,
+      child: PageView.builder(
+          pageSnapping: false,
+          controller: PageController(
+            viewportFraction: 0.3,
+            initialPage: 1,
+          ),
+          itemCount: actores.length,
+          itemBuilder: (context, i) => _actorTarjeta(context,actores[i])),
+    );
+  }
+
+  Widget _actorTarjeta(BuildContext context ,Actor actor) {
+    return Container(
+      margin: EdgeInsets.only(left:10.0),
+      child: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+                      child: FadeInImage(
+              placeholder: AssetImage('assets/img/no-image.jpg'),
+              image: NetworkImage(actor.getImageProfile()),
+              fit: BoxFit.cover,
+              fadeInDuration: Duration(milliseconds: 150),   
+              height: 150.0,       ),
+          ),
+          Text(actor.name,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.subhead,
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -1,71 +1,70 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
 
 class MovieHorizontal extends StatelessWidget {
-
   final List<Pelicula> peliculas;
 
-  final _pageController = PageController(
-     initialPage: 1,
-    viewportFraction: 0.3
-  );
+  final _pageController = PageController(initialPage: 1, viewportFraction: 0.3);
 
   final Function siguientePagina;
 
-MovieHorizontal({@required this.peliculas, @required this.siguientePagina});
+  MovieHorizontal({@required this.peliculas, @required this.siguientePagina});
 
   @override
   Widget build(BuildContext context) {
     final _screenSize = MediaQuery.of(context).size;
-    _pageController.addListener((){
-        
-      if ( _pageController.position.pixels >= _pageController.position.maxScrollExtent - 200) {
-           siguientePagina();  
+    _pageController.addListener(() {
+      if (_pageController.position.pixels >=
+          _pageController.position.maxScrollExtent - 200) {
+        siguientePagina();
       }
     });
 
     return Container(
-           height: _screenSize.height * 0.30,
+      height: _screenSize.height * 0.30,
       child: PageView.builder(
-        pageSnapping: false,
-        controller: _pageController,
-        itemCount: peliculas.length,
-        itemBuilder:(context, i) => _tarjeta(context, peliculas[i])
-      ),
+          pageSnapping: false,
+          controller: _pageController,
+          itemCount: peliculas.length,
+          itemBuilder: (context, i) => _tarjeta(context, peliculas[i])),
     );
   }
 
-  Widget _tarjeta (BuildContext context, Pelicula pelicula){
+  Widget _tarjeta(BuildContext context, Pelicula pelicula) {
     final tarjeta = Container(
-               margin: EdgeInsets.only(right: 15.0),
-               child: Column(
-                 children: <Widget>[
+      margin: EdgeInsets.only(right: 15.0),
+      child: Column(
+        children: <Widget>[
+          Hero(
+            tag: pelicula.id,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+                image: NetworkImage(pelicula.getPosterImg()),
+                placeholder: AssetImage('assets/img/no-image.jpg'),
+                fit: BoxFit.cover,
+                height: 160.0,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Text(
+            pelicula.title,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.caption,
+          )
+        ],
+      ),
+    );
 
-                   ClipRRect(
-                     borderRadius: BorderRadius.circular(20.0),
-                       child: FadeInImage(
-                       image: NetworkImage(pelicula.getPosterImg()),
-                       placeholder: AssetImage('assets/img/no-image.jpg'), 
-                       fit: BoxFit.cover,
-                       height: 160.0,
-                     ),
-                   ),
-                   SizedBox(height: 5.0,),
-                   Text(
-                     pelicula.title,
-                     overflow: TextOverflow.ellipsis,
-                     style: Theme.of(context).textTheme.caption,
-                   )
-                 ],
-               ),
-             );
-
-          return GestureDetector(
-            onTap: (){
-              Navigator.pushNamed(context, 'detalle', arguments: pelicula);
-            }, 
-            child: tarjeta,
-          );
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+      },
+      child: tarjeta,
+    );
   }
 
 /*  List<Widget> _tarjetas(BuildContext context) {
